181d08f3124a5cd03681ff588bfc43f0d3a29ec4
DemetrioPadre <demetriopadre@gmail.com>
2024-03-22 17:54:48 +0100

    bonus portato a casa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,35 @@
 @section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <section>
-    <div class="container py-4">
-      <h1>Laravel with Bootstrap + Vite</h1>
-    </div>
-  </section>
+    <section>
+        <div class="container py-4">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">name </th>
+                        <th scope="col">slug </th>
+                        <th scope="col">type</th>
+                        <th scope="col">category </th>
+                        <th scope="col">weight </th>
+                        <th scope="col">cost </th>
+                        <th scope="col">damage_dice </th>
+                    </tr>
+
+                <tbody>
+                    @foreach ($items as $item)
+                        <tr>
+                            <td scope="col">{{ $item->name }}</td>
+                            <td scope="col">{{ $item->slug }}</td>
+                            <td scope="col">{{ $item->type }}</td>
+                            <td scope="col">{{ $item->category }}</td>
+                            <td scope="col">{{ $item->weight }}</td>
+                            <td scope="col">{{ $item->cost }}</td>
+                            <td scope="col">{{ $item->damage_dice }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+
 @endsection
