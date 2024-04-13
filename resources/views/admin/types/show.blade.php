ce77878947c9379a0295db2414e2867015f50457
lucapolzone <lucapolzone@gmail.com>
2024-04-13 10:18:27 +0200

    fix: show() syntax for Type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -8,28 +8,16 @@
 
             <div class="card">
 
-                <h2 class="card-header text-center text-uppercase">{{$type['name']}}</h2>
+                <h2 class="card-header text-center text-uppercase">{{$type->name}}</h2>
 
                 <div class=" card-body d-flex my-3 gap-5">
                     <div class="card-img">
-                        <img src="{{$type['image']}}" alt="img-type">
+                        <img src="{{$type->image}}" alt="img-type">
                     </div>
-                    <p class="d-flex align-items-center">{{$type['description']}}</p>
+                    <p class="d-flex align-items-center">{{$type->description}}</p>
                 </div>
-                {{-- <div class="text-light d-flex gap-5 align-items-center justify-content-center" >
-                    <div>Price: {{$comic['price']}}</div>
-                    <div>Series: {{$comic['series']}}</div>
-                    <div>Sale Date: {{$comic['sale_date']}}</div>
-                    <div>Type: {{$comic['type']}}</div>
-                </div> --}}
+
             </div>
-            {{-- <div class="d-flex justify-content-center my-3"> --}}
-                {{-- <button class="btn btn-primary rounded-0 custom-btn"> --}}
-                    {{-- <a class="text-light" href="{{route('comics.edit', $comic['id'])}}">Modifica</a> --}}
-                {{-- </button> --}}
-            {{-- </div> --}}
-
-
 
 
         </div>
