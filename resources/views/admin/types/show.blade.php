897974f12e27717f99ad49bf321710f6130b453e
lucapolzone <lucapolzone@gmail.com>
2024-04-13 10:16:28 +0200

    feat: finish show() for Type resource

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -5,12 +5,33 @@
 @section('content')
     <section>
         <div class="container py-4">
-            <strong>Nome: </strong> {{ $type->name }} <br />
-            {{-- <strong>Immagine: </strong> {{ $type->image }} <br /> --}}
-            <img src="{{ asset($type->image) }}" alt="">
-            <p>
-                <strong>Descrizione: </strong> {{ $type->description }} <br />
-            </p>
+
+            <div class="card">
+
+                <h2 class="card-header text-center text-uppercase">{{$type['name']}}</h2>
+
+                <div class=" card-body d-flex my-3 gap-5">
+                    <div class="card-img">
+                        <img src="{{$type['image']}}" alt="img-type">
+                    </div>
+                    <p class="d-flex align-items-center">{{$type['description']}}</p>
+                </div>
+                {{-- <div class="text-light d-flex gap-5 align-items-center justify-content-center" >
+                    <div>Price: {{$comic['price']}}</div>
+                    <div>Series: {{$comic['series']}}</div>
+                    <div>Sale Date: {{$comic['sale_date']}}</div>
+                    <div>Type: {{$comic['type']}}</div>
+                </div> --}}
+            </div>
+            {{-- <div class="d-flex justify-content-center my-3"> --}}
+                {{-- <button class="btn btn-primary rounded-0 custom-btn"> --}}
+                    {{-- <a class="text-light" href="{{route('comics.edit', $comic['id'])}}">Modifica</a> --}}
+                {{-- </button> --}}
+            {{-- </div> --}}
+
+
+
+
         </div>
     </section>
     <div class="container">
